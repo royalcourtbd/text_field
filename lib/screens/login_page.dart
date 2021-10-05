651db79cf0e29810ec78d808285ef9dee4a14967
MD ABU SAYED <sayedvai08@gmail.com>
2021-10-06 01:24:57 +0600

    login & registration page work

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:text_field/screens/registration_page.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,10 +9,148 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  bool isPassIsVisible = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _emailController.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage('images/bg.png'),fit: BoxFit.fill),
+          ),
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: TextField(
+                  autocorrect: true,
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Type Your Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(28.0),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.grey,
+                    ),
+                    suffixIcon: _emailController.text.isEmpty
+                        ? Container(
+                            width: 0,
+                          )
+                        : IconButton(
+                            padding: EdgeInsets.only(right: 14),
+                            alignment: Alignment.centerRight,
+                            icon: Icon(Icons.close),
+                            onPressed: () => _emailController.clear(),
+                          ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: TextField(
+                  controller: _passController,
+                  maxLength: 8,
+                  obscureText: isPassIsVisible,
+                  autocorrect: true,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Type Your Password',
+                    border: OutlineInputBorder(
+                      gapPadding: 25.0,
+                      borderRadius: BorderRadius.circular(28.0),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.password,
+                      color: Colors.grey,
+                    ),
+                    suffixIcon: IconButton(
+                      padding: EdgeInsets.only(right: 14),
+                      alignment: Alignment.centerRight,
+                      icon: IconButton(
+                        icon: isPassIsVisible?Icon(
+                          Icons.visibility_off,
+                        ):Icon(Icons.visibility),
+                        onPressed: () {
+                          setState(() {
+                            isPassIsVisible = !isPassIsVisible;
+                          });
+                        },
+                      ),
+                      onPressed: () => _passController.clear(),
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  //fixedSize: Size(200, 50),
+                  padding:
+                      EdgeInsets.only(right: 35, left: 35, top: 12, bottom: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      28.0,
+                    ),
+                  ),
+                ),
+
+                //style: ButtonStyle(fixedSize: ),
+                onPressed: () {},
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 19,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Don\'t have an Account?'),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => RegistrationPage(),
+                        ),
+                      );
+                    },
+                    child: Text('Click here'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -6,8 +6,201 @@ class RegistrationPage extends StatefulWidget {
 }
 
 class _RegistrationPageState extends State<RegistrationPage> {
+  final _emailController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _passController = TextEditingController();
+  bool isPassIsVisible = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _nameController.addListener(() {
+      setState(() {});
+    });
+    _emailController.addListener(() {
+      setState(() {});
+    });
+
+    _phoneController.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage('images/bg.png'),fit: BoxFit.fill),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: TextField(
+                  controller: _nameController,
+                  autocorrect: true,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                    hintText: 'Type Your Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(28.0),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: Colors.grey,
+                    ),
+                    suffixIcon: _nameController.text.isEmpty
+                        ? Container(
+                            width: 0,
+                          )
+                        : IconButton(
+                            padding: EdgeInsets.only(right: 14),
+                            alignment: Alignment.centerRight,
+                            icon: Icon(Icons.close),
+                            onPressed: () => _nameController.clear(),
+                          ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: TextField(
+                  controller: _phoneController,
+                  autocorrect: true,
+                  maxLength: 11,
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    labelText: 'Phone',
+                    hintText: 'Type Your Number',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(28.0),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.phone,
+                      color: Colors.grey,
+                    ),
+                    suffixIcon: _phoneController.text.isEmpty
+                        ? Container(
+                            width: 0,
+                          )
+                        : IconButton(
+                            padding: EdgeInsets.only(right: 14),
+                            alignment: Alignment.centerRight,
+                            icon: Icon(Icons.close),
+                            onPressed: () => _phoneController.clear(),
+                          ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: TextField(
+                  autocorrect: true,
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Type Your Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(28.0),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.grey,
+                    ),
+                    suffixIcon: _emailController.text.isEmpty
+                        ? Container(
+                      width: 0,
+                    )
+                        : IconButton(
+                      padding: EdgeInsets.only(right: 14),
+                      alignment: Alignment.centerRight,
+                      icon: Icon(Icons.close),
+                      onPressed: () => _emailController.clear(),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: TextField(
+                  controller: _passController,
+                  maxLength: 8,
+                  obscureText: isPassIsVisible,
+                  autocorrect: true,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Type Your Password',
+                    border: OutlineInputBorder(
+                      gapPadding: 25.0,
+                      borderRadius: BorderRadius.circular(28.0),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.password,
+                      color: Colors.grey,
+                    ),
+                    suffixIcon: IconButton(
+                      padding: EdgeInsets.only(right: 14),
+                      alignment: Alignment.centerRight,
+                      icon: IconButton(
+                        icon: isPassIsVisible?Icon(
+                          Icons.visibility_off,
+                        ):Icon(Icons.visibility),
+                        onPressed: () {
+                          setState(() {
+                            isPassIsVisible = !isPassIsVisible;
+                          });
+                        },
+                      ),
+                      onPressed: () => _passController.clear(),
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  //fixedSize: Size(200, 50),
+                  padding:
+                      EdgeInsets.only(right: 35, left: 35, top: 12, bottom: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      28.0,
+                    ),
+                  ),
+                ),
+
+                //style: ButtonStyle(fixedSize: ),
+                onPressed: () {},
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 19,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
